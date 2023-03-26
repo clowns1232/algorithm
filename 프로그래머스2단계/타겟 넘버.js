4398b4dd042a1e19e5bd438993fea9d2f36c7ab5
Hwang taehyeun <clowns1232@gmail.com>
2023-03-26 23:10:36 +0900

    프로그래머스 깊이우선 너비 우선 탐색 문제

---
+++ 프로그래머스2단계/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+  let answer = 0;
+  dfs(0, 0);
+  function dfs(x, value) {
+    if (x < numbers.length) {
+      dfs(x + 1, value + numbers[x]);
+      dfs(x + 1, value - numbers[x]);
+    } else {
+      if (value === target) {
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
+
+solution([1, 1, 1, 1, 1], 3);
